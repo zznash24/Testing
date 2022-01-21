a5b253771f69bc2ee2e3381277ccddc9a7696161
Zach Nash <afreakenturtle@gmail.com>
2022-01-20 20:57:45 -0500

    scripts.js

--- assignments-cli/git_folder/scripts.js
+++ assignments-cli/git_folder/scripts.js
@@ -1,10 +1,9 @@
-console.log ("friends")
-
-
 const friends = ["jenn", "morgan", "justin"]
 
 let i = 0;
 
 for (let i = 0; i < 3; i++) {
-    friends += i;
+    friends = friends + i;
 }
+
+console.log(friends);
