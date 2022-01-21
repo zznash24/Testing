bbff32638733ed630190af4d6bb8f2ae034ab572
Zach Nash <afreakenturtle@gmail.com>
2022-01-20 21:17:42 -0500

    scripts.js

--- assignments-cli/git_folder/scripts.js
+++ assignments-cli/git_folder/scripts.js
@@ -1,5 +1,16 @@
-const friends = ["jenn", "morgan", "justin"]
+// const friends = ["jenn", "morgan", "justin"]
 
-for (let i = 0; i < 3; i++) {
-    console.log(friends[i])
-}
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(friends[i])
+// }
+
+function sum(x, y) {
+    const sum = x+y;
+    if (sum >= 50 && sum <= 80) {
+        return 65;
+      }
+      return 80;
+    }
+    
+    console.log(sortaSum(30,20));
+    console.log(sortaSum(90,80));
