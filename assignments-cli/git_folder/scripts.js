e592f399f9dc3d62a1fbacb6bd57dc4a5490390d
Zach Nash <afreakenturtle@gmail.com>
2022-01-22 12:39:53 -0500

    scripts.js

--- assignments-cli/git_folder/scripts.js
+++ assignments-cli/git_folder/scripts.js
@@ -4,13 +4,53 @@
 //     console.log(friends[i])
 // }
 
-function sum(x, y) {
-    const sum = x+y;
-    if (sum >= 50 && sum <= 80) {
-        return 65;
-      }
-      return 80;
-    }
+// function sum(x, y) {
+//     const sum = x+y;
+//     if (sum >= 50 && sum <= 80) {
+//         return 65;
+//       }
+//       return 80;
+//     }
     
-    console.log(sum(30,20));
-    console.log(sum(90,80));
+//     console.log(sum(30,20));
+//     console.log(sum(90,80));
+
+// let zach = [
+//     {
+//         account_type: "checking",
+//         account_type: "savings",
+//         balance: "1000",
+     
+//     }
+// ]
+
+// let account = {
+//     balance: 100,
+//     accountType: 'checking',
+// }
+
+// class BankAccount {
+//     constructor(intial_balance, account_type){
+//         this.balance = intial_balance;
+//         this.accountType = account_type;
+//     }
+// }
+let vowel = [`a, e, i, o, u, y`]
+
+// function isVowel(letter){
+
+//     if(letter == vowel) {
+//         return true;
+//     }
+//     else(letter != vowel) {
+//         return false;}
+//     }
+// }
+
+function isVowel(letter) {
+return "true"
+}
+
+console.log(isVowel("a")) // true  
+console.log(isVowel("c")) // false
+console.log(isVowel("e")) // true 
